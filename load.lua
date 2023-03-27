d027a25487b1aa9d8c1d561b91fa92a3215bb1db
K1nscripts <122467621+K1nscripts@users.noreply.github.com>
2023-03-27 22:23:35 +0200

    Create load.lua

---
+++ load.lua
@@ -0,0 +1,30 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame.Position = UDim2.new(-2.04982519, 0, -1.60493827, 0)
+Frame.Size = UDim2.new(0, 10000, 0, 6000)
+
+TextLabel.Parent = ScreenGui
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.255303413, 0, 0.35061729, 0)
+TextLabel.Size = UDim2.new(0, 900, 0, 242)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "LOADING"
+TextLabel.TextColor3 = Color3.fromRGB(0, 255, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
